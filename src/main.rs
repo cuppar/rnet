f599470877ee4b58985b10d0d036c17f71e33426
cuppar <cuppar@163.com>
2023-12-15 18:54:16 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{sync::mpsc, thread};
+
 fn main() {
-    println!("Hello, world!");
+    let rhs = vec![0, 10, 20, 30, 40, 50];
+    let lhs = vec![0, 1, 2, 3, 4, 5];
+    let (tx, rx) = mpsc::channel();
+    let mut handles = vec![];
+    
+    for i in 0..rhs.len() {
+        let rhs = rhs.clone();
+        let lhs = lhs.clone();
+        let tx = tx.clone();
+        let handle = thread::spawn(move || {
+            let s = format!(
+                "Thread {} added {} and {}, result {}",
+                i,
+                rhs[i],
+                lhs[i],
+                rhs[i] + lhs[i]
+            );
+            tx.send(s).unwrap();
+        });
+        handles.push(handle);
+    }
+
+    // let _ = handles.into_iter().map(|h| h.join());
+    drop(tx);
+    for result in rx {
+        println!("{}", result);
+    }
 }
